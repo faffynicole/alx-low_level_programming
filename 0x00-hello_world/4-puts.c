f9f0a4d35bb491e533655f9c4be943a628ce5ed0
faffynicole <nicole.kurwa6@gmail.com>
2022-06-08 15:48:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,8 @@
- /*
-  * File: 4-puts.c
-  *Auth: faffynicole  
-  */
-
   #include <stdio.h>
 
-  /**
-  * main - Prints "Programming is like bulding a multilingual
- *                puzzle, followed by a new line.
- *
- * Return: Always 0.
- */4 int main(void)
+   int main(void)
  {
-  char str1 [Programming is like bulding a multilingual puzzle];
+  	char str1 [Programming is like bulding a multilingual puzzle];
 	puts(Programming is like bulding a multilingual puzzle);
          return (0);
  }
